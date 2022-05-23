6a598554a8c05eac5e348eff4d376428f2cfb0b1
galfrylich <galfrylich@gmail.com>
2022-05-23 14:41:17 +0300

    add function to check password

---
+++ password-validator.sh
@@ -0,0 +1,31 @@
+#!/bin/bash    
+
+password=$1
+len="${#password}"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+check_pass(){
+    
+    if [[ $len -ge 10 ]]
+    then
+        if [[ $password =~ [[:alpha:]] && $password =~ [[:digit:]] ]]; then 
+            if [[ "$password" =~ [[:upper:]] ]] && [[ "$password" =~ [[:lower:]] ]]; then 
+                    echo -e "${GREEN}password is good"
+                    exit 0
+                    
+            else 
+                    echo -e "${RED}password must include both the small and capital case letters. $?"
+                    exit 1
+                fi        
+        else 
+            echo -e "${RED}password must Contain both alphabet and number $?" 
+            exit 1
+        fi       
+    else
+    echo -e "${RED}password must contain minimum of 10 characters $?"
+    exit 1
+    fi
+}
+check_pass password
